9126cc8f54d26a82bfec55ec8c18a6bf6f4a37bc
Tony Brix <Tony@Brix.ninja>
2017-03-06 00:45:02 -0600

    update specs

--- spec/reporter-spec-v1.js
+++ spec/reporter-spec-v1.js
@@ -7,83 +7,95 @@ function deprecatedFunction() {
 	Grim.deprecate("This function is deprecated! Please use `nonDeprecatedFunction()`");
 }
 
-describe("passing", function () {
-	it("should pass", function () {
-		expect(true).toBe(true);
-	});
-	it("should pass async", function () {
-		waitsForPromise(_ => new Promise(resolve => {
-			jasmine.unspy(window, "setTimeout");
-			setTimeout(_ => {
-				expect(true).toBe(true);
-				resolve();
-			}, 1000);
-		}));
-	});
-	it("should pass with no function");
-});
-describe("pending", function () {
-	xit("should be pending", function () {
-		expect(true).toBe(true);
-	});
-});
-describe("failing", function () {
-	it("should fail", function () {
-		expect(true).toBe(false);
-	});
-	it("should fail async", function () {
-		waitsForPromise(_ => new Promise(resolve => {
-			jasmine.unspy(window, "setTimeout");
-			setTimeout(_ => {
-				expect(true).toBe(false);
-				resolve();
-			}, 1000);
-		}));
-	});
-});
-describe("deprecations", function () {
-	it("should report deprecation", function () {
-		expect(true).toBe(true);
-		Grim.deprecate("This has been deprecated!");
+describe("Jasmine 1.x", function () {
+	beforeEach(function () {
+		jasmine.useRealClock();
 	});
 
-	it("should report deprecation async", function () {
-		waitsForPromise(_ => new Promise(resolve => {
-			jasmine.unspy(window, "setTimeout");
-			setTimeout(_ => {
-				expect(true).toBe(true);
-				deprecatedFunction();
-				resolve();
-			}, 1000);
-		}));
-	});
-
-	it("should report deprecation from failure", function () {
-		deprecatedFunction();
-		expect(true).toBe(false);
-		deprecatedFunction();
-	});
-});
-describe("multiple suites", function () {
 	describe("passing", function () {
 		it("should pass", function () {
 			expect(true).toBe(true);
 		});
+
+		it("should pass async", function () {
+			waitsForPromise(_ => new Promise(resolve => {
+				setTimeout(_ => {
+					expect(true).toBe(true);
+					resolve();
+				}, 1000);
+			}));
+		});
+
+		it("should pass with no function");
 	});
 	describe("pending", function () {
 		xit("should be pending", function () {
 			expect(true).toBe(true);
 		});
 	});
+
 	describe("failing", function () {
 		it("should fail", function () {
 			expect(true).toBe(false);
 		});
+
+		it("should fail async", function () {
+			waitsForPromise(_ => new Promise(resolve => {
+				setTimeout(_ => {
+					expect(true).toBe(false);
+					resolve();
+				}, 1000);
+			}));
+		});
 	});
-	describe("deprecation", function () {
-		it("should be deprecated", function () {
-			deprecatedFunction();
+
+	describe("deprecations", function () {
+		it("should report deprecation", function () {
 			expect(true).toBe(true);
+			Grim.deprecate("This has been deprecated!");
+		});
+
+		it("should report deprecation async", function () {
+			waitsForPromise(_ => new Promise(resolve => {
+				setTimeout(_ => {
+					expect(true).toBe(true);
+					deprecatedFunction();
+					resolve();
+				}, 1000);
+			}));
+		});
+
+		it("should report deprecation from failure", function () {
+			deprecatedFunction();
+			expect(true).toBe(false);
+			deprecatedFunction();
+		});
+	});
+
+	describe("multiple suites", function () {
+		describe("passing", function () {
+			it("should pass", function () {
+				expect(true).toBe(true);
+			});
+		});
+
+		describe("pending", function () {
+			xit("should be pending", function () {
+				expect(true).toBe(true);
+			});
+		});
+
+		describe("failing", function () {
+			it("should fail", function () {
+				expect(true).toBe(false);
+			});
+		});
+
+		describe("deprecation", function () {
+			it("should be deprecated", function () {
+				deprecatedFunction();
+				expect(true).toBe(true);
+			});
 		});
 	});
 });
--- spec/reporter-spec.js
+++ spec/reporter-spec.js
@@ -7,107 +7,107 @@ function deprecatedFunction() {
 	Grim.deprecate("This function is deprecated! Please use `nonDeprecatedFunction()`");
 }
 
-describe("passing", function () {
-	it("should pass", function () {
-		expect(true).toBe(true);
-	});
-	it("should pass async", function (done) {
-		// jasmine.useRealClock();
-		setTimeout(_ => {
-			expect(true).toBe(true);
-			done();
-		}, 1000);
-	});
-});
-
-describe("passing", function () {
-	it("should pass", function () {
-		expect(true).toBe(true);
-	});
-	it("should pass async", function (done) {
-		// jasmine.useRealClock();
-		setTimeout(_ => {
-			expect(true).toBe(true);
-			done();
-		}, 1000);
-	});
-});
-
-describe("pending", function () {
-	xit("should be pending", function () {
-		expect(true).toBe(true);
-	});
-	it("should be pending when called", function () {
-		pending("because i called pending");
-		expect(true).toBe(true);
-	});
-	it("should be pending with no function");
-});
-describe("failing", function () {
-	it("should fail", function () {
-		expect(true).toBe(false);
-	});
-	it("should fail async", function (done) {
-		// jasmine.useRealClock();
-		setTimeout(_ => {
-			expect(true).toBe(false);
-			done();
-		}, 1000);
-	});
-	it("should fail when called", function () {
-		fail("because i called fail");
-		expect(true).toBe(false);
-	});
-	it("should fail when called async", function (done) {
-		// jasmine.useRealClock();
-		setTimeout(_ => {
-			fail("because i called fail async");
-			expect(true).toBe(false);
-			done();
-		}, 1000);
-	});
-});
-describe("deprecations", function () {
-	it("should report deprecation", () => {
-		expect(true).toBe(true);
-		Grim.deprecate("This has been deprecated!");
+describe("Jasmine 2.x", function () {
+	beforeEach(function () {
+		jasmine.useRealClock();
 	});
 
-	it("should report deprecation async", function (done) {
-		// jasmine.useRealClock();
-		setTimeout(_ => {
-			expect(true).toBe(true);
-			deprecatedFunction();
-			done();
-		}, 1000);
-	});
-
-	it("should report deprecation from failure", () => {
-		deprecatedFunction();
-		expect(true).toBe(false);
-		deprecatedFunction();
-	});
-});
-describe("multiple suites", function () {
 	describe("passing", function () {
 		it("should pass", function () {
 			expect(true).toBe(true);
 		});
+
+		it("should pass async", function (done) {
+			setTimeout(_ => {
+				expect(true).toBe(true);
+				done();
+			}, 1000);
+		});
 	});
+
 	describe("pending", function () {
 		xit("should be pending", function () {
 			expect(true).toBe(true);
 		});
+
+		it("should be pending when called", function () {
+			pending("because i called pending");
+			expect(true).toBe(true);
+		});
+
+		it("should be pending with no function");
 	});
+
 	describe("failing", function () {
 		it("should fail", function () {
 			expect(true).toBe(false);
 		});
+
+		it("should fail async", function (done) {
+			setTimeout(_ => {
+				expect(true).toBe(false);
+				done();
+			}, 1000);
+		});
+
+		it("should fail when called", function () {
+			fail("because i called fail");
+			expect(true).toBe(false);
+		});
+
+		it("should fail when called async", function (done) {
+			setTimeout(_ => {
+				fail("because i called fail async");
+				expect(true).toBe(false);
+				done();
+			}, 1000);
+		});
 	});
-	describe("deprecation", function () {
-		it("should be deprecated", function () {
-			deprecatedFunction();
+	describe("deprecations", function () {
+		it("should report deprecation", () => {
 			expect(true).toBe(true);
+			Grim.deprecate("This has been deprecated!");
+		});
+
+		it("should report deprecation async", function (done) {
+			setTimeout(_ => {
+				expect(true).toBe(true);
+				deprecatedFunction();
+				done();
+			}, 1000);
+		});
+
+		it("should report deprecation from failure", () => {
+			deprecatedFunction();
+			expect(true).toBe(false);
+			deprecatedFunction();
+		});
+	});
+
+	describe("multiple suites", function () {
+		describe("passing", function () {
+			it("should pass", function () {
+				expect(true).toBe(true);
+			});
+		});
+
+		describe("pending", function () {
+			xit("should be pending", function () {
+				expect(true).toBe(true);
+			});
+		});
+
+		describe("failing", function () {
+			it("should fail", function () {
+				expect(true).toBe(false);
+			});
+		});
+
+		describe("deprecation", function () {
+			it("should be deprecated", function () {
+				deprecatedFunction();
+				expect(true).toBe(true);
+			});
 		});
 	});
 });
