73f02afd52fb51515469f01cf19b729b5f761c49
Tony Brix <Tony@Brix.ninja>
2017-03-07 14:40:22 -0600

    add attach-to-dom tests

---
+++ spec/spec-helper/attach-to-dom-spec.js
@@ -0,0 +1,19 @@
+describe("attach-to-dom", function () {
+		beforeEach(function () {
+			this.jasmine2Content = document.getElementById("jasmine2-content");
+		});
+
+		it("should add jasmine.attachToDOM", function () {
+			expect(jasmine.attachToDOM).toEqual(jasmine.any(Function));
+		});
+
+		it("should attach #jasmine2-content to the dom", function () {
+			expect(this.jasmine2Content).toBeInDOM();
+		});
+
+		it("should append to #jasmine2-content", function () {
+			const element = document.createElement("div");
+			jasmine.attachToDOM(element);
+			expect(this.jasmine2Content).toContainElement(element);
+		});
+});
