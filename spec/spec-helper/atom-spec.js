2c8d54d869afa7eab8079e63f837edcabca07f46
Tony Brix <Tony@Brix.ninja>
2017-03-08 16:57:28 -0600

    add atom tests

---
+++ spec/spec-helper/atom-spec.js
@@ -0,0 +1,14 @@
+/* globals atom */
+
+describe("atom", function () {
+	it("should spy on atom.menu.sendToBrowserProcess", function () {
+		expect(jasmine.isSpy(atom.menu.sendToBrowserProcess)).toBe(true);
+	});
+
+	it("should set default config", function () {
+		expect(atom.config.get("core.destroyEmptyPanes")).toBe(false);
+		expect(atom.config.get("editor.fontFamily")).toBe("Courier");
+		expect(atom.config.get("editor.fontSize")).toBe(16);
+		expect(atom.config.get("editor.autoIndent")).toBe(false);
+	});
+});
