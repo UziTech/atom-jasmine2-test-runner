ce1f0972ca8d93f061d8779a0c6f98a91447903e
Tony Brix <Tony@Brix.ninja>
2017-03-08 12:54:47 -0600

    add fixture tests

---
+++ spec/spec-helper/fixtures-spec.js
@@ -0,0 +1,14 @@
+"use babel";
+/* globals atom */
+
+import path from "path";
+
+describe("fixtures", function () {
+	it("should set paths to the fixtures folder", function () {
+		expect(atom.project.getPaths()).toContain(path.resolve(__dirname, "../fixtures"));
+	});
+
+	it("should resolve this package to this path", function () {
+		expect(path.resolve(atom.packages.resolvePackagePath("atom-jasmine2-test-runner"))).toBe(path.resolve(__dirname, "../../"));
+	});
+});
