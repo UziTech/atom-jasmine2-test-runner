80e2b053b2ae2b9f78e968ecaa03d56e0868b031
Tony Brix <Tony@Brix.ninja>
2017-03-09 09:54:34 -0600

    remove attach-to-dom spec reliance on jasmine-jquery

--- spec/spec-helper/attach-to-dom-spec.js
+++ spec/spec-helper/attach-to-dom-spec.js
@@ -1,19 +1,21 @@
 describe("attach-to-dom", function () {
-		beforeEach(function () {
-			this.jasmine2Content = document.getElementById("jasmine2-content");
-		});
+	beforeEach(function () {
+		this.jasmine2Content = document.getElementById("jasmine2-content");
+	});
 
-		it("should define jasmine.attachToDOM", function () {
-			expect(jasmine.attachToDOM).toEqual(jasmine.any(Function));
-		});
+	it("should define jasmine.attachToDOM", function () {
+		expect(jasmine.attachToDOM).toEqual(jasmine.any(Function));
+	});
 
-		it("should attach jasmine2-content to the dom", function () {
-			expect(this.jasmine2Content).toBeInDOM();
-		});
+	it("should attach jasmine2-content to the dom", function () {
 
-		it("should append to jasmine2-content", function () {
-			const element = document.createElement("div");
-			jasmine.attachToDOM(element);
-			expect(this.jasmine2Content).toContainElement(element);
-		});
+		// uses jasmine-jquery
+		expect(this.jasmine2Content instanceof HTMLElement).toBe(true);
+	});
+
+	it("should append to jasmine2-content", function () {
+		const element = document.createElement("div");
+		jasmine.attachToDOM(element);
+		expect(this.jasmine2Content.contains(element)).toBe(true);
+	});
 });
