e6485aa306ff73771a4b4da0f2ca8e59cc1add61
Tony Brix <Tony@Brix.ninja>
2017-05-22 23:19:12 -0500

    add pathwatcher test

---
+++ spec/spec-helper/pathwatcher-spec.js
@@ -0,0 +1,22 @@
+"use babel";
+
+import pathwatcher from "pathwatcher";
+
+describe("pathwatcher", function () {
+	beforeAll(function () {
+		spyOn(console, "error");
+	});
+	it("should pass", function () {
+		const pw = pathwatcher.watch("../");
+		pw.close();
+	});
+	it("should report error", function(){
+		expect(console.error).not.toHaveBeenCalled();
+	});
+	it("should fail", function () {
+		pathwatcher.watch("../");
+	});
+	it("should report error", function(){
+		expect(console.error).toHaveBeenCalledTimes(1);
+	});
+});
